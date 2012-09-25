c788aca25ce3f75bcbfdaef9fae99c5e00250250
Geoff Pleiss <gpleiss@gmail.com>
2012-09-24 22:02:55 -0400

    Made linear classifier work, refactored code, made first pass at prediction

---
+++ calculate_linear_average.m
@@ -0,0 +1,34 @@
+
+function calculate_linear_average()
+    %================================
+    % Calculates the average image for digits 0-9.
+    % Saves the result in a matrix - result is persisted in file average.mat
+    % Column 1 is the actual digit value
+    % Column 2 is the number of samples for this digit
+    % Columns 3:786 are the average pixel values for pixels 1-784, read left to right, top to bottom
+    %================================
+
+    data = csvread('train.csv', [1,0,30000,0]); %(:,1) is the actual digit value  
+
+    digits = data(:,1);
+    pixels = data(:,2:end);
+
+
+    index = linspace(0, 9, 10);
+    results = zeros(10, 785);
+    result = [index', results]; 
+
+    for k=1:size(digits)
+      actual = digits(k);
+      row_index = actual + 1;
+
+      result(row_index, 3:end) = result(row_index, 3:end) .+ pixels(k, :);
+      result(row_index, 2) = result(row_index, 2) + 1;
+    end
+
+    for i=1:10
+      result(i, 3:end) = result(i, 3:end) / result(i, 2);
+    end
+
+    save average.mat result
+end
--- linear_classifier.m
+++ linear_classifier.m
@@ -1,49 +1,36 @@
-data = csvread('train.csv', [1,0,30000,784]); %(:,1) is the actual digit value  
 
-digits = data(:,1);
-% pixels = data(:,2:end);
-
-% index = linspace(0, 9, 10);
-% results = zeros(10, 785);
-% result = [index', results]; %first column is digit, second is count, 3:786 are pixel averages
-
-% for k=1:size(digits)
-%   actual = digits(k);
-%   row_index = actual + 1;
-
-%   result(row_index, 3:end) = result(row_index, 3:end) .+ pixels(k, :);
-%   result(row_index, 2) = result(row_index, 2) + 1;
-% end
-
-% for i=1:10
-%   result(i, 3:end) = result(i, 3:end) / result(i, 2);
-% end
-
-% save average.mat result
 
 load('average.mat');
-test_data = csvread('test.csv');
+test_data = csvread('test.csv', [1, 0, 28000, 783]);
 
 total_tries = 0
 total_correct = 0
 
+digit_guesses = zeros(28000, 1);
+
 for i=1:size(test_data)
-  min_cost = [1000000000000000, 100];
+  min_cost = [1000000000000000, 100]; %[cost, prediction]
+  actual = test_data(i, :);
+
   for j=1:10
     average = result(j, 3:end);
-    actual = test_data(i, :);
+    
     cost = sum((average - actual) .^2)/(2*784);
     if (cost < min_cost(1))
       min_cost = [cost, j-1];
     end
   end
-  
-  total_tries += 1;
-  if (min_cost(2) == digits(i))
-    total_correct += 1;
-  end
+
+  digit_guesses(i) = min_cost(2);
+
+  % total_tries += 1;
+  % if (min_cost(2) == digits(i))
+  %   total_correct += 1;
+  % end
 end
 
-total_tries
-total_correct
-total_correct/ total_tries
+csvwrite('prediction.csv', digit_guesses);
+
+% total_tries
+% total_correct
+% total_correct/ total_tries
