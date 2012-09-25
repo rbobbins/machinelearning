dfff70f7ac117ad907a15f310f037619b2c5cdf2
Rachel <rachelheidi@gmail.com>
2012-09-24 21:04:33 -0400

    First try at classifying digits. 10% success rate

--- linear_classifier.m
+++ linear_classifier.m
@@ -1,35 +1,50 @@
-clc;
-clear all;
-close all;
 
-data = csvread('train.csv', [1,0,100,784]); %(:,1) is the actual digit value  
+data = csvread('train.csv', [1,0,30000,784]); %(:,1) is the actual digit value  
 
 digits = data(:,1);
-pixels = data(:,2:end);
+% pixels = data(:,2:end);
 
-zero_digits = [];
-for k=1:100
-  actual = digits(k);
-  if actual == 1
-    zero_digits(end+1, 1:784) = pixels(k, :);
+% index = linspace(0, 9, 10);
+% results = zeros(10, 785);
+% result = [index', results]; %first column is digit, second is count, 3:786 are pixel averages
+
+% for k=1:size(digits)
+%   actual = digits(k);
+%   row_index = actual + 1;
+
+%   result(row_index, 3:end) = result(row_index, 3:end) .+ pixels(k, :);
+%   result(row_index, 2) = result(row_index, 2) + 1;
+% end
+
+% for i=1:10
+%   result(i, 3:end) = result(i, 3:end) / result(i, 2);
+% end
+
+% save average.mat result
+
+load('average.mat');
+test_data = csvread('test.csv');
+
+total_tries = 0
+total_correct = 0
+
+for i=1:size(test_data)
+  min_cost = [1000000000000000, 100];
+  for j=1:10
+    average = result(j, 3:end);
+    actual = test_data(i, :);
+    cost = sum((average - actual) .^2)/(2*784);
+    if (cost < min_cost(1))
+      min_cost = [cost, j-1];
+    end
+  end
+  
+  total_tries += 1;
+  if (min_cost(2) == digits(i))
+    total_correct += 1;
   end
 end
 
-zero_average = mean(zero_digits, 1);
-
-figure();
-axis square;
-xlim([1 28]);
-ylim([1 28]);
-
-hold on;
-for i=0:27
-  for j=1:28
-    rgb = zero_average(28*i + j);
-    if (rgb != 0)
-      hold on;
-      color = (1 - rgb/256) * [1,1,1];
-      plot(j,28-i, "markeredgecolor", color, "markerfacecolor", color, "marker", 'o');
-    end
-  end
-end 
+total_tries
+total_correct
+total_correct/ total_tries
